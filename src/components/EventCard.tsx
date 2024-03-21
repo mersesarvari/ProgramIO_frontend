751e97b15900b1c11df81e5ec9adcf58e64e38ca
nazmox <merse.sarvari1@gmail.com>
2024-03-21 15:40:24 +0100

    added basic card view

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,3 +1,5 @@
+import { Rating } from "flowbite-react";
+
 const EventCard = () => {
   return (
     <div className="card-compact w-90">
@@ -11,8 +13,15 @@ const EventCard = () => {
       <div className="card-body items-left text-left">
         <div className="px-0 py-0 mx-2 mt-0">
           <h2 className="card-title">
-            Shoes!
-            <div className="badge badge-secondary">NEW</div>
+            <div className="items-center w-full font-bold text-gray-900 dark:text-white">
+              Shoes!
+            </div>
+            <Rating className="">
+              <Rating.Star />
+              <p className="mx-1 text-sm font-bold text-gray-900 dark:text-white text-left items-center">
+                4.95
+              </p>
+            </Rating>
           </h2>
           <p>Proba header</p>
           <p>If a dog chews shoes whose shoes does he choose?</p>
