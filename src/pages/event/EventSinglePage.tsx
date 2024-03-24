2050f784c4bf219c2f58112bc98a1a8d62525372
nazmox <merse.sarvari1@gmail.com>
2024-03-24 13:25:49 +0100

    added basic rating panel

--- src/pages/event/EventSinglePage.tsx
+++ src/pages/event/EventSinglePage.tsx
@@ -105,6 +105,58 @@ const EventSinglePage = () => {
             <div className="flex flex-col w-full">
               <hr className="w-full h-0.5 mx-auto my-4 bg-gray-200 border-0 rounded md:my-10 dark:bg-gray-700" />
             </div>
+            {/* Statistics */}
+            <div className="stats shadow w-full">
+              <div className="stat">
+                <div className="stat-figure text-primary">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block w-8 h-8 stroke-current"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    ></path>
+                  </svg>
+                </div>
+                <div className="stat-title">Total Likes</div>
+                <div className="stat-value text-primary">25.6K</div>
+                <div className="stat-desc">21% more than last month</div>
+              </div>
+
+              <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block w-8 h-8 stroke-current"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M13 10V3L4 14h7v7l9-11h-7z"
+                    ></path>
+                  </svg>
+                </div>
+                <div className="stat-title">Page Views</div>
+                <div className="stat-value text-secondary">2.6M</div>
+                <div className="stat-desc">21% more than last month</div>
+              </div>
+
+              <div className="stat">
+                <div className="stat-value">86%</div>
+                <div className="stat-title">Tasks done</div>
+                <div className="stat-desc text-secondary">
+                  31 tasks remaining
+                </div>
+              </div>
+            </div>
           </div>
           {/* Left information column */}
           <div className="col-span-2 pl-10">
@@ -151,6 +203,7 @@ const EventSinglePage = () => {
               </div>
             </div>
           </div>
+          {/* Google maps Row */}
           <div className="col-span-5">
             <GoogleMapSingleMarker
               width={"100%"}
