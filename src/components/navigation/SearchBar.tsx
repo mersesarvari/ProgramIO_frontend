ca6edd9a8a3984c599b24c9d403a7433a0de79d5
nazmox <merse.sarvari1@gmail.com>
2024-04-04 21:27:27 +0200

    added basic search bar

--- src/components/navigation/SearchBar.tsx
+++ src/components/navigation/SearchBar.tsx
@@ -4,7 +4,7 @@ const Searchbard = () => {
       <div className="bg-gray-400 w-5/12 h-[68px] rounded-full flex flex-row border-gray-400 border-2">
         <div className="basis-1/2 bg-white rounded-l-full border-r-2 border-gray-400">
           <div className="basis-1/2 bg-white rounded-l-full size-full rounded-full hover:bg-gray-200">
-            <div className="flex">
+            <div className="flex items-center">
               <div className="h-12 w-12 bg-red-600 rounded-full"></div>
               <div className="m-auto text-left mx-8 py-2.5">
                 <div className="text-sm font-bold text-black">Where</div>
