fe5d7313dfd9ae2198edb28e02c85e28e1d25261
nazmox <merse.sarvari1@gmail.com>
2024-04-04 21:26:23 +0200

    added searchbar

--- src/components/navigation/SearchBar.tsx
+++ src/components/navigation/SearchBar.tsx
@@ -4,17 +4,20 @@ const Searchbard = () => {
       <div className="bg-gray-400 w-5/12 h-[68px] rounded-full flex flex-row border-gray-400 border-2">
         <div className="basis-1/2 bg-white rounded-l-full border-r-2 border-gray-400">
           <div className="basis-1/2 bg-white rounded-l-full size-full rounded-full hover:bg-gray-200">
-            <div className="m-auto text-left mx-8">
-              <div className="text-sm font-bold text-black">Where</div>
-              <div className="text-sm font-normal text-gray">
-                Search locations
+            <div className="flex">
+              <div className="h-12 w-12 bg-red-600 rounded-full"></div>
+              <div className="m-auto text-left mx-8 py-2.5">
+                <div className="text-sm font-bold text-black">Where</div>
+                <div className="text-sm font-normal text-gray">
+                  Search locations
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="basis-1/4 bg-white border-r-2 border-gray-400">
           <div className="basis-1/2 bg-white hover:rounded-full size-full  hover:bg-gray-200">
-            <div className="m-auto text-left mx-8">
+            <div className="m-auto text-left mx-4 py-2.5">
               <div className="text-sm font-bold text-black">When</div>
               <div className="text-sm font-normal text-gray">Add date</div>
             </div>
@@ -22,12 +25,20 @@ const Searchbard = () => {
         </div>
         <div className="basis-1/4 bg-white border-r-2 border-gray-400">
           <div className="basis-1/2 bg-white hover:rounded-full size-full  hover:bg-gray-200">
-            Harmadik
+            <div className="m-auto text-left mx-4 py-2.5">
+              <div className="text-sm font-bold text-black">What</div>
+              <div className="text-sm font-normal text-gray">do you need</div>
+            </div>
           </div>
         </div>
         <div className="basis-1/2 bg-white rounded-r-full">
           <div className="basis-1/2 bg-white rounded-r-full size-full hover:rounded-full  hover:bg-gray-200">
-            Negyedik
+            <div className="m-auto text-left mx-4 py-2.5">
+              <div className="text-sm font-bold text-black">Looking for</div>
+              <div className="text-sm font-normal text-gray">
+                something specific
+              </div>
+            </div>
           </div>
         </div>
       </div>
