b0aa4a9793b8e8c7a6338ccacda8daf135e9f5d5
nazmox <merse.sarvari1@gmail.com>
2024-04-05 19:11:41 +0200

    added hover cover for the first button

--- src/components/navigation/SearchBar.tsx
+++ src/components/navigation/SearchBar.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from "react";
 
 const Searchbard = () => {
   const [openedIndex, setOpenedIndex] = useState(null);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
   const ref = useRef(null);
 
   useEffect(() => {
@@ -18,24 +19,32 @@ const Searchbard = () => {
     };
   }, []);
 
+  useEffect(() => {
+    console.log("Hovered index:", hoveredIndex);
+  }, [hoveredIndex]);
+
   const getButtonStyles = (index: number) => {
     let styles = "";
     //When one menu is active but not this one
     if (openedIndex !== index && openedIndex !== null) {
       styles +=
-        "bg-gray-200 border-gray-200 hover:bg-gray-300 hover:border-gray-300";
+        "rounded-full bg-gray-200 border-gray-200 hover:bg-gray-300 hover:border-gray-300 ";
     }
     //When menu is active
     if (openedIndex === index) {
-      styles += ` border-gray-400 bg-white z-500`;
+      styles += `rounded-full border-gray-400 bg-white`;
     }
     //When all menu is inactive
     if (openedIndex === null) {
-      styles += ` hover:bg-gray-200 hover:border-gray-300 border-white `;
+      styles += `rounded-full bg-gray-200 hover:bg-gray-200 hover:border-gray-300 border-white `;
     }
-    //When next is active
-    if (openedIndex === index + 1) {
-      styles += `w-[300px]`;
+
+    if (
+      hoveredIndex === index &&
+      openedIndex !== null &&
+      openedIndex !== index
+    ) {
+      styles += `rounded-l-full`;
     }
 
     return `${styles} w-full`;
@@ -50,71 +59,93 @@ const Searchbard = () => {
       >
         {/* SearchBar */}
         <div
-          className={`w-8/12 xl:w-6/12 h-[72px] ${
+          className={`w-8/12 xl:w-6/12 h-[72px] z-400 my-10 ${
             openedIndex === null ? " bg-white" : "bg-gray-200"
           } rounded-full flex flex-row border-gray-300 border-2 size-full group`}
         >
           {/* Button 1 */}
-          <button
-            className={`basis-4/12 rounded-full border-x-2 ${getButtonStyles(
-              1
-            )}`}
-            onClick={() => setOpenedIndex(1)}
-            tabIndex={1}
+          <div
+            className={`${
+              openedIndex === 2 && hoveredIndex === 1
+                ? "basis-4/12 flex flex-row rounded-full bg-gray-300"
+                : "basis-4/12 flex flex-row rounded-full"
+            }`}
           >
-            <div className="m-auto text-left ml-6 py-auto">
-              <div className="text-sm font-bold text-black">Where</div>
-              <div className="text-sm font-normal text-gray">
-                Search locations
+            <button
+              className={`basis-1/2 border-x-2 ${getButtonStyles(1)}`}
+              onClick={() => setOpenedIndex(1)}
+              onMouseEnter={() => {
+                setHoveredIndex(1);
+              }}
+              onMouseLeave={() => {
+                setHoveredIndex(null);
+              }}
+              tabIndex={1}
+            >
+              <div className="m-auto text-left ml-6 py-auto">
+                <div className="text-sm font-bold text-black">Where</div>
+                <div className="text-sm font-normal text-gray">
+                  Search locations
+                </div>
               </div>
-            </div>
-          </button>
+            </button>
 
-          {/* Button 2 */}
-          <button
-            className={`basis-2/12 rounded-full border-x-2 ${getButtonStyles(
-              2
-            )}`}
-            onClick={() => setOpenedIndex(2)}
-            tabIndex={2}
-          >
-            <div className="m-auto text-left ml-6 py-auto">
-              <div className="text-sm font-bold text-black">Where</div>
-              <div className="text-sm font-normal text-gray">
-                Search locations
+            {/* Button 2 */}
+            <button
+              className={`basis-1/2  rounded-full border-x-2 ${getButtonStyles(
+                2
+              )}`}
+              onClick={() => setOpenedIndex(2)}
+              onMouseEnter={() => {
+                setHoveredIndex(2);
+              }}
+              onMouseLeave={() => {
+                setHoveredIndex(null);
+              }}
+              tabIndex={2}
+            >
+              <div className="m-auto text-left ml-6 py-auto">
+                <div className="text-sm font-bold text-black">Where</div>
+                <div className="text-sm font-normal text-gray">
+                  Search locations
+                </div>
               </div>
-            </div>
-          </button>
+            </button>
+          </div>
           {/* Button 3 */}
-          <button
-            className={`basis-2/12 rounded-full border-x-2 ${getButtonStyles(
-              3
-            )}`}
-            onClick={() => setOpenedIndex(3)}
-            tabIndex={3}
-          >
-            <div className="m-auto text-left ml-6 py-auto">
-              <div className="text-sm font-bold text-black">Where</div>
-              <div className="text-sm font-normal text-gray">
-                Search locations
+          <div className="basis-2/12 rounded-full ">
+            <button
+              className={`size-full rounded-full border-x-2 ${getButtonStyles(
+                3
+              )}`}
+              onClick={() => setOpenedIndex(3)}
+              tabIndex={3}
+            >
+              <div className="m-auto text-left ml-6 py-auto">
+                <div className="text-sm font-bold text-black">Where</div>
+                <div className="text-sm font-normal text-gray">
+                  Search locations
+                </div>
               </div>
-            </div>
-          </button>
+            </button>
+          </div>
           {/* Button 4 */}
-          <button
-            className={`basis-4/12 rounded-full border-x-2 ${getButtonStyles(
-              4
-            )}`}
-            onClick={() => setOpenedIndex(4)}
-            tabIndex={4}
-          >
-            <div className="m-auto text-left ml-6 py-auto">
-              <div className="text-sm font-bold text-black">Where</div>
-              <div className="text-sm font-normal text-gray">
-                Search locations
+          <div className="basis-4/12 rounded-full">
+            <button
+              className={`size-full rounded-full border-x-2 ${getButtonStyles(
+                4
+              )}`}
+              onClick={() => setOpenedIndex(4)}
+              tabIndex={4}
+            >
+              <div className="m-auto text-left ml-6 py-auto">
+                <div className="text-sm font-bold text-black">Where</div>
+                <div className="text-sm font-normal text-gray">
+                  Search locations
+                </div>
               </div>
-            </div>
-          </button>
+            </button>
+          </div>
         </div>
       </div>
     </>
